6d2cc2c03cd0405aa59dfc23cafc428f2c21a9df
IrelDev <ireldeveloper@gmail.com>
2021-09-15 21:09:15 +0500

    Add initial xcodeproject

---
+++ Heads-Hands-Test-Assignment/Heads-Hands-Test-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Heads-Hands-Test-Assignment
+//
+//  Created by Kirill Pustovalov on 15.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
