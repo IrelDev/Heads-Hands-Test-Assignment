082fe933f3cbb91a283b0a990d9bd4ae6517402a
IrelDev <ireldeveloper@gmail.com>
2021-09-16 21:08:06 +0500

    Add AuthenticationService usage

--- Heads-Hands-Test-Assignment/Heads-Hands-Test-Assignment/Controllers/LoginViewController.swift
+++ Heads-Hands-Test-Assignment/Heads-Hands-Test-Assignment/Controllers/LoginViewController.swift
@@ -8,16 +8,51 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    let loginView = LoginView()
+    private let loginView = LoginView()
+    private lazy var authenticationService = AuthenticationService()
     
     override func loadView() {
         view = loginView
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupTargets()
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+        
+    func setupTargets() {
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        authenticationService.delegate = self
+    }
+    @objc func loginButtonTapped() {
+        authenticationService.wakeUpSession()
+    }
 }
 
+extension LoginViewController: AuthenticationServiceDelegate {
+    func authenticationSucceeded() {
+        let newsFeedViewController = NewsFeedViewController()
+        navigationController?.setViewControllers([newsFeedViewController], animated: true)
+    }
+    func authenticationFailed() {
+        let alert = UIAlertController(title: "Error", message: "Authentication Failed", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(alertAction)
+        
+        SceneDelegate.getTopMostViewController()?.present(alert, animated: true, completion: nil)
+    }
+    
+    func shouldShowViewController(viewController: UIViewController) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+}
