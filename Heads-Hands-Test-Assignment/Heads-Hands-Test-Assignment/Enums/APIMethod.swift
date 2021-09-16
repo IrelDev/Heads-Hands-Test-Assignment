5fb955892c5a10ab8a2872cc3c01477206951496
IrelDev <ireldeveloper@gmail.com>
2021-09-16 20:33:00 +0500

    Add APIMethod enum

---
+++ Heads-Hands-Test-Assignment/Heads-Hands-Test-Assignment/Enums/APIMethod.swift
@@ -0,0 +1,32 @@
+//
+//  APIMethod.swift
+//  Heads-Hands-Test-Assignment
+//
+//  Created by Kirill Pustovalov on 16.09.2021.
+//
+
+import Foundation
+import VKSdkFramework
+
+enum APIMethod: String {
+    case news = "/method/newsfeed.get"
+}
+extension APIMethod {
+    func getVKApiURL(with parameters: [String: String] = [:]) -> URL? {
+        var components = URLComponents()
+        
+        components.scheme = "https"
+        components.host = "api.vk.com"
+        components.path = rawValue
+        
+        var allParameters = parameters
+        allParameters["v"] = "5.131"
+        allParameters["access_token"] = VKSdk.accessToken()?.accessToken
+        
+        components.queryItems = allParameters.map {
+            URLQueryItem(name: $0, value: $1)
+        }
+        
+        return components.url
+    }
+}
