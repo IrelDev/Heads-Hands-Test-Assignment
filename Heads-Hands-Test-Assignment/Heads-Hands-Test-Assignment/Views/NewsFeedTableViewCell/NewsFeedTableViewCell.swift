055316d53aebc63305cdb917757eb87a6d930758
IrelDev <ireldeveloper@gmail.com>
2021-09-17 20:27:00 +0500

    Add NewsFeedTableViewCell collection view

--- Heads-Hands-Test-Assignment/Heads-Hands-Test-Assignment/Views/NewsFeedTableViewCell/NewsFeedTableViewCell.swift
+++ Heads-Hands-Test-Assignment/Heads-Hands-Test-Assignment/Views/NewsFeedTableViewCell/NewsFeedTableViewCell.swift
@@ -11,10 +11,21 @@ class NewsFeedTableViewCell: UITableViewCell {
     var headerView = NewsFeedHeaderView()
     var footerView = NewsFeedFooterView()
     
-    var somethingView: UIView = {
-        let view = UIView()
+    var attachments: [Attachment]?
+    
+    var contentCollectionViewConstraint: NSLayoutConstraint!
+    
+    var contentCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
         
-        return view
+        let contentCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        contentCollectionView.backgroundColor = .clear
+        contentCollectionView.isScrollEnabled = false
+        
+        contentCollectionView.showsHorizontalScrollIndicator = false
+        contentCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        return contentCollectionView
     }()
     lazy var stackView: UIStackView = {
         let stackView = UIStackView()
@@ -39,6 +50,7 @@ class NewsFeedTableViewCell: UITableViewCell {
         super.prepareForReuse()
         
         headerView.reset()
+        attachments = nil
     }
 }
 extension NewsFeedTableViewCell {
@@ -47,18 +59,83 @@ extension NewsFeedTableViewCell {
         separatorInset = UIEdgeInsets.zero
         layoutMargins = UIEdgeInsets.zero
         
+        contentView.autoresizingMask = [.flexibleHeight]
         contentView.addSubview(stackView)
         
         stackView.addArrangedSubview(headerView)
-        stackView.addArrangedSubview(somethingView)
+        stackView.addArrangedSubview(contentCollectionView)
         stackView.addArrangedSubview(footerView)
+        
+        contentCollectionView.delegate = self
+        contentCollectionView.dataSource = self
+        contentCollectionView.register(NewsFeedCollectionViewCell.self, forCellWithReuseIdentifier: "attachmentCell")
     }
     func setupNSLayoutConstraints() {
+        let bottomConstraint = stackView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: -10)
+        bottomConstraint.priority = UILayoutPriority(999)
+        
+        contentCollectionViewConstraint = contentCollectionView.heightAnchor.constraint(equalToConstant: 0)
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor, constant: -10),
-            stackView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor, constant: 10),
-            stackView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: -10)
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            
+            bottomConstraint,
+            contentCollectionViewConstraint
         ])
+    }
+}
+extension NewsFeedTableViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let attachments = attachments else { return CGSize.zero }
+        var sizes = CGSize()
+        
+        let width = bounds.width
+        
+        if indexPath.row == 0 || indexPath.row == attachments.count - 1 && indexPath.row % 2 != 0 {
+            var ratio: CGFloat = 1.0
+            
+            let attachment = attachments[indexPath.row]
+            
+            if let photo = attachment.photo {
+                ratio = CGFloat(photo.height) / CGFloat(photo.width)
+            } else if let video = attachment.video {
+                ratio = CGFloat(video.imageHeight) / CGFloat(video.imageWidth)
+            }
+            
+            let height = width * CGFloat(ratio)
+            sizes = CGSize(width: width, height: height)
+        } else {
+            sizes = CGSize(width: bounds.width / 2 - 2, height: 160)
+        }
+        return sizes
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        2
+    }
+}
+extension NewsFeedTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        attachments?.count ?? 0
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "attachmentCell", for: indexPath) as! NewsFeedCollectionViewCell
+        guard let model = attachments?[indexPath.row] else { return UICollectionViewCell() }
+        
+        if let photo = model.photo {
+            if let url = URL(string: photo.url) {
+                cell.imageView.kf.setImage(with: url)
+            }
+        } else if let video = model.video {
+            if let url = URL(string: video.imageUrl) {
+                cell.imageView.kf.setImage(with: url)
+            }
+        }
+        return cell
     }
 }
